3dc0d11c41d4241564923fbfa21f39b27c36fdfb
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2025-12-27 09:19:28 +0530

    Add folder

---
+++ Front-end/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SignUppage from "./Component/SignUppage";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<SignUppage />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
---
+++ Front-end/src/Component/SignUppage.jsx
@@ -0,0 +1,33 @@
+import React  from "react";
+
+const SignUppage = () => {
+    return (
+        <div className="signuppage-container">
+            <h2>Sign Up</h2>
+            <form className="signup-form">
+                <div className="form-group">
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div className="form-group">
+
+                    <label htmlFor="email">Email:</label>   
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="confirm-password">Confirm Password:</label>
+                    <input type="password" id="confirm-password" name="confirm-password" required />
+                </div>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+};
+
+export default SignUppage;
+
+                
